0708a7ca97f5dfc0e5151aa4cdf972fd0974f80f
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-24 11:34:55 +0700

    feat: register page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,12 +1,17 @@
+import { AuthCard } from '@/components'
+import RegisterFragments from '@/components/molecules/Auth/RegisterFragments'
+import { setTitle } from '@/utils'
 import {Cookies} from 'react-cookie'
+
+export const metadata = setTitle('Regiser')
 
 type Props = {}
 
 const RegisterPage = (props: Props) => {
   return (
-    <div>
-
-    </div>
+    <AuthCard reverse={true} url='/img/student-2.jpg'>
+      <RegisterFragments/>
+    </AuthCard>
   )
 }
 
